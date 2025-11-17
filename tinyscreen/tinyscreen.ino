e730e30c5e21c144299a73622b8d472bed8a2021
canairo <qqqdyz@gmail.com>
2025-11-17 17:38:43 +0800

    wifi nonsense now working

--- tinyscreen/tinyscreen.ino
+++ tinyscreen/tinyscreen.ino
@@ -10,60 +10,98 @@
 
 TinyScreen display = TinyScreen(TinyScreenPlus);
 
-void handle_wifi() {
-  int num_networks = WiFi.scanNetworks();
-  SerialUSB.println(WiFi.firmwareVersion());
-  display.println("scanning for wifi...");
-  display.setCursor(0, 10);
-  SerialUSB.print("wifi network debugging\n-----\n\n");
-  for (int i = 0; i < num_networks; i++) {
-    serialf("network %d: %s %d %d",
-      i,
-      WiFi.SSID(i),
-      WiFi.RSSI(i),
-      WiFi.encryptionType(i)
-    );
-  }
-  SerialUSB.println("-----");
-  char ssid[64];
-  char pass[64];
-  display.println("prompting for creds (on serial)");
-  SerialUSB.print("enter SSID > ");
-  read_line(ssid, MAXLEN);
-  SerialUSB.print("enter password > ");
-  read_line(pass, MAXLEN);
-  serialf("trying to connect to %s with %s", ssid, pass);
-  unsigned long start = millis();
-  while (WiFi.status() != WL_CONNECTED && millis() - start < 15000) { // 15s timeout
-      SerialUSB.print(".");
-      delay(500);
-  }
+bool prompt_and_connect() {
+    display.clearScreen();
+    display.setCursor(0, 0);
+    display.println("meowing for the wifi...");
+    SerialUSB.println("\nmeowing for networks...");
 
-  if (WiFi.status() != WL_CONNECTED) {
-      SerialUSB.println("\nFailed to connect");
-  } else {
-      SerialUSB.print("\nConnected! IP: ");
-      SerialUSB.println(WiFi.localIP());
-  }
+    int num_networks = WiFi.scanNetworks();
+    if (num_networks == 0) {
+        SerialUSB.println("no networks found :(");
+        display.println("no networks found :(");
+        delay(2000);
+        return false;
+    }
 
+    for (int i = 0; i < num_networks; i++) {
+        serialf("[%d] %s (RSSI %d) ENC %d", i, WiFi.SSID(i), WiFi.RSSI(i), WiFi.encryptionType(i));
+    }
+
+    SerialUSB.print("\npick one > ");
+    char input[MAXLEN];
+    read_line(input, MAXLEN);
+    int choice = atoi(input);
+    if (choice < 0 || choice >= num_networks) {
+        SerialUSB.println("kena...");
+        return false;
+    }
+
+    char ssid[MAXLEN];
+    strncpy(ssid, WiFi.SSID(choice), MAXLEN);
+    ssid[MAXLEN - 1] = '\0';
+
+    char pass[MAXLEN] = {0};
+    int enc = WiFi.encryptionType(choice);
+    if (enc != ENC_TYPE_NONE) {
+        SerialUSB.print("enter password > ");
+        read_line(pass, MAXLEN);
+    }
+
+    serialf("connecting to %s...", ssid);
+    display.clearScreen();
+    display.setCursor(0, 0);
+    display.println("da connecterrr...");
+
+    WiFi.disconnect();
+    delay(100);
+    WiFi.begin(ssid, pass);
+
+    unsigned long start = millis();
+    while (WiFi.status() != WL_CONNECTED && millis() - start < WIFITIMEOUT) {
+        SerialUSB.print(".");
+        delay(500);
+    }
+
+    if (WiFi.status() != WL_CONNECTED) {
+        SerialUSB.println("\nkena :(");
+        display.clearScreen();
+        display.setCursor(0, 0);
+        display.println("connection kena :(");
+        delay(2000);
+        return false;
+    } else {
+        SerialUSB.print("\nyay! ip: ");
+        SerialUSB.println(WiFi.localIP());
+        display.clearScreen();
+        display.setCursor(0, 0);
+        display.println("wahoo!");
+        return true;
+    }
 }
 
-void setup(void) {
-  Wire.begin();//initialize I2C before we can initialize TinyScreen- not needed for TinyScreen+
-  SerialUSB.begin(9600);
-  WiFi.setPins(8, 2, A3, -1);
-  display.begin();
-  display.setBrightness(10);
-  display.clearScreen();
-  display.setFont(thinPixel7_10ptFontInfo);
-  display.setCursor(0, 0);
-  delay(5000); // bit of time so that the serial can actually connect
-  handle_wifi();
+void setup() {
+    Wire.begin();
+    SerialUSB.begin(9600);
+    while (!SerialUSB);
+
+    WiFi.setPins(8, 2, A3, -1);
+    display.begin();
+    display.setBrightness(10);
+    display.clearScreen();
+    display.setFont(thinPixel7_10ptFontInfo);
+    display.setCursor(0, 0);
+    delay(2000);
 }
 
 void loop() {
-  display.clearScreen();
-  display.println("waiting for input...");
-  delay(1000);
-  display.setCursor(0, 0);
+    if (WiFi.status() != WL_CONNECTED) {
+        prompt_and_connect();
+    } else {
+        display.clearScreen();
+        display.setCursor(0, 0);
+        display.println("Connected!");
+        delay(5000);
+    }
 }
+
